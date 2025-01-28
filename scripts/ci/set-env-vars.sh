13caeccdbff8ba0172d29b853d9e52fd22c45f42
sunlei <guizaicn@gmail.com>
2025-01-29 00:03:58 +0800

    ci: add workflow

--- scripts/ci/set-env-vars.sh
+++ scripts/ci/set-env-vars.sh
@@ -5,30 +5,8 @@ set -euo pipefail
   echo "BUILD_MODE=release"
 
   # > --------------------------------------------------
-  # > cargo
-  echo "CARGO_TARGET_DIR=${GITHUB_WORKSPACE}/.cache/cargo/target"
-  # echo "CARGO_LOG=debug" >> "${GITHUB_ENV}"
-  # echo "CARGO_TERM_VERBOSE=true" >> "${GITHUB_ENV}"
-  echo "RUST_BACKTRACE=1"
-
-  if [[ "${TARGET_ARCH}" != "aarch64-apple-darwin" ]]; then
-    echo "CARGO_BUILD_TARGET=${TARGET_ARCH}"
-    echo "RUSTFLAGS=-C target-cpu=${TARGET_CPU}"
-  fi
-
-  # -Z tune-cpu=${TARGET_CPU} # the option `Z` is only accepted on the nightly compiler
-  # if [[ "${TARGET_ARCH}" == "aarch64-apple-darwin" ]]; then
-  #   echo "CARGO_BUILD_TARGET=${TARGET_ARCH}"
-  #   echo "RUSTFLAGS=-C target-cpu=${TARGET_CPU} -C link-arg=-undefined -C link-arg=dynamic_lookup"
-  # else
-  #   echo "CARGO_BUILD_TARGET=${TARGET_ARCH}"
-  #   echo "RUSTFLAGS=-C target-cpu=${TARGET_CPU}"
-  # fi
-
-  # > --------------------------------------------------
   # > sccache
   # https://github.com/Mozilla-Actions/sccache-action
-
   echo "SCCACHE_IDLE_TIMEOUT=0"
   echo "SCCACHE_DIRECT=true"
   echo "SCCACHE_CACHE_MULTIARCH=1"
@@ -39,4 +17,20 @@ set -euo pipefail
   # Incrementally compiled crates cannot be cached by sccache
   # https://github.com/mozilla/sccache#rust
   echo "CARGO_INCREMENTAL=0"
+
+  # > --------------------------------------------------
+  # > cargo
+  echo "CARGO_TARGET_DIR=${GITHUB_WORKSPACE}/.cache/cargo/target"
+  # echo "CARGO_LOG=debug"
+  # echo "CARGO_TERM_VERBOSE=true"
+  echo "RUST_BACKTRACE=1"
+
+  echo "CARGO_BUILD_TARGET=${TARGET_ARCH}"
+
+  # -Z tune-cpu=${TARGET_CPU} # the option `Z` is only accepted on the nightly compiler
+
+  if [[ "${TARGET_CPU}" != "" ]]; then
+    echo "RUSTFLAGS=-C target-cpu=${TARGET_CPU}"
+  fi
+
 } >>"${GITHUB_ENV}"
