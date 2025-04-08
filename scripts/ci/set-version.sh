4084cf708eb11cf99b980afd226a02cc7c9f9897
sunlei <guizaicn@gmail.com>
2025-04-08 18:23:06 +0800

    fix: build

--- scripts/ci/set-version.sh
+++ scripts/ci/set-version.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # Format: {semver}.dev{date}+{build_number}.cpu.{target_cpu}
 # Example: 1.208.0.dev20251212+7001.cpu.sapphirerapids
 
-base_version=$(yq -r '.tool.poetry.version' src/pyproject.toml)
+base_version=$(yq -r '.project.version' src/pyproject.toml)
 
 dev_version="dev$(date -u +%Y%m%d)+${GITHUB_RUN_NUMBER}"
 release_version=${base_version}.${dev_version}
@@ -16,7 +16,7 @@ else
   new_version="${release_version}.${local_version_cpu}"
 fi
 
-poetry version --directory=src "${new_version}"
+# poetry version --directory=src "${new_version}"
 echo "RELEASE_VERSION=${release_version}" >>"$GITHUB_ENV"
 
 echo "Set version to ${new_version}"
